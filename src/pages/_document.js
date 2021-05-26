fe3d0c0b9a4022357bf349eadc17b1640ff608de
Cristian González <kazragore96@gmail.com>
2021-05-25 19:31:09 -0500

    Agregadas meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,26 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#0a518f" />
+          <meta
+            name="description"
+            content="Tendencias educativas hacia el futuro digital de la educación universitaria"
+          />
+          <meta
+            property="og:description"
+            content="Tendencias educativas hacia el futuro digital de la educación universitaria"
+          />
+          <meta
+            property="og:image"
+            content="https://imagenes.softaki.com/app/Minedu/conectados-wsp.jpg"
+          />
+          <meta property="og:title" content="I Congreso Conectados" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <title>I Congreso Conectados</title>
           <link
             as="font"
             rel="preload"
